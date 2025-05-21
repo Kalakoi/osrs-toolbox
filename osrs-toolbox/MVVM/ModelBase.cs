d51b227ca64d89017970b8630382eb04f42bfb68
Kalakoi <kalakoi@kalakoigaming.com>
2025-05-21 12:06:16 -0400

    Added Model-View-ViewModel framework support.

---
+++ osrs-toolbox/MVVM/ModelBase.cs
@@ -0,0 +1,17 @@
+﻿namespace osrs_toolbox
+{
+    public abstract class ModelBase : PropertyChangedBase
+    {
+        public void SetProperty<T>(ref T storage, T value, string name)
+        {
+            if (storage == null)
+                throw new InvalidCastException("osrs_toolbox.ModelBase.UpdateProperty\nStorage cannot be null.");
+            if (value == null)
+                throw new InvalidCastException("osrs_toolbox.ModelBase.UpdateProperty\nValue cannot be null.");
+            if (storage.GetType() != value.GetType())
+                throw new InvalidCastException("osrs_toolbox.ModelBase.UpdateProperty\nVariable type mismatch between storage and value.");
+            storage = value;
+            OnPropertyChanged(name);
+        }
+    }
+}
