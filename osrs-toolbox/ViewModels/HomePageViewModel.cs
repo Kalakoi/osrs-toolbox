5356e1a541cce0d4a20651f5386c8a3991e8a404
Kalakoi <kalakoi@kalakoigaming.com>
2025-05-22 11:13:29 -0400

    Added variables to home page for calling competition overlay.

--- osrs-toolbox/ViewModels/HomePageViewModel.cs
+++ osrs-toolbox/ViewModels/HomePageViewModel.cs
@@ -16,12 +16,26 @@ namespace osrs_toolbox
 
         private void InitializeVariables()
         {
-
+            CompetitionID = -1;
+            GroupID = -1;
+            PlayerName = string.Empty;
+            HideOtherPlayers = false;
+            HideZeroKC = false;
         }
 
         private void InitializeCommands()
         {
+            ToggleCompetitionOverlay = new RelayCommand(DoToggleCompetitionOverlay);
+        }
 
+        private void DoToggleCompetitionOverlay(object obj)
+        {
+            if (CompetitionOverlayView.Current != null)
+                CompetitionOverlayView.Current.Close();
+            else
+            {
+                CompetitionOverlayView cov = new CompetitionOverlayView();
+            }
         }
     }
 }
