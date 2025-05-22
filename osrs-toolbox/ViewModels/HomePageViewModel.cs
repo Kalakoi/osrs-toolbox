c4370af08c483cdc8bfba3224053835622c5484e
Kalakoi <kalakoi@kalakoigaming.com>
2025-05-22 10:49:02 -0400

    Created home page view to act as the main landing for the application.

---
+++ osrs-toolbox/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osrs_toolbox
+{
+    public class HomePageViewModel : HomePageModel
+    {
+        public HomePageViewModel()
+        {
+            InitializeVariables();
+            InitializeCommands();
+        }
+
+        private void InitializeVariables()
+        {
+
+        }
+
+        private void InitializeCommands()
+        {
+
+        }
+    }
+}
