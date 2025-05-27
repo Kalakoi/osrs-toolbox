2154a7d58be7fad6a858ba620c93bb5318ccccd6
Kalakoi <kalakoi@kalakoigaming.com>
2025-05-27 13:31:18 -0400

    Added another test endpoint.

--- osrs-toolbox/APIs/Wiki/Wiki.cs
+++ osrs-toolbox/APIs/Wiki/Wiki.cs
@@ -10,6 +10,6 @@ namespace osrs_toolbox
     public static class Wiki
     {
         private static string BaseEndpoint = @"https://oldschool.runescape.wiki/api.php?format=json&formatversion=2";
-
+        private static string ListMonstersEndpoint = @"&action=query&list=categorymembers&cmtitle=Category:Monsters";
     }
 }
