75195e2c954894022783ac44e6567feca04ecd5b
Kalakoi <kalakoi@kalakoigaming.com>
2025-06-10 16:25:42 -0400

    Started integrating RANDOM.ORG API for competition rolling.

---
+++ osrs-toolbox/Random.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osrs_toolbox
+{
+    public static class Random
+    {
+        private static string BaseEndpoint = @"https://api.randdom.org/json-rpc/4/invoke";
+        private static string APIKey = APIKeys.RANDOM;
+    }
+}
