7c7b80277d88eb8d456bfb2170031855d41cf054
Kalakoi <kalakoi@kalakoigaming.com>
2025-06-19 07:21:52 -0400

    Added template for APIKeys class.

---
+++ osrs-toolbox/APIs/APIKeys.template.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osrs_toolbox
+{
+    public static class APIKeys
+    {
+        public static readonly string RANDOM = "";
+    }
+}
