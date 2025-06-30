723a3d8a03d15ba24c544529791405f06357d767
Kalakoi <kalakoi@kalakoigaming.com>
2025-06-30 11:12:29 -0400

    Additional testing of streamlined GUI elements.

--- osrs-toolbox/Controls/ToolboxWindow.xaml.cs
+++ osrs-toolbox/Controls/ToolboxWindow.xaml.cs
@@ -24,5 +24,35 @@ namespace osrs_toolbox
         {
             InitializeComponent();
         }
+
+        private void Move_Window(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed) { ((this.Parent as Grid).Parent as Window).DragMove(); }
+        }
+
+        private void Close_Window(object sender, MouseButtonEventArgs e)
+        {
+            ((this.Parent as Grid).Parent as Window).Close();
+        }
+
+        private void Button_Hovered(object sender, MouseEventArgs e)
+        {
+            (sender as Image).Source = new BitmapImage(new Uri(@"/Resources/wood-button-pressed.png", UriKind.Relative));
+        }
+
+        private void Button_Unhovered(object sender, MouseEventArgs e)
+        {
+            (sender as Image).Source = new BitmapImage(new Uri(@"/Resources/wood-button.png", UriKind.Relative));
+        }
+
+        private void Exit_Hovered(object sender, MouseEventArgs e)
+        {
+            (sender as Rectangle).Fill = new SolidColorBrush(Colors.Red);
+        }
+
+        private void Exit_Unhovered(object sender, MouseEventArgs e)
+        {
+            (sender as Rectangle).Fill = new SolidColorBrush(Colors.White);
+        }
     }
 }
